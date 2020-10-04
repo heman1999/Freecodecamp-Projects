f12e3d4344765fcd8f23a462426d9afd8620a9d4
Himanshu Upadhyay <heman6760@gmail.com>
2020-10-04 16:08:49 +0530

    add Timestapm microservice

---
+++ APIs and Microservices Projects/Timestamp Microservice/server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  let inputDate = req.params.date_string;
+
+  if (!inputDate) {
+    res.send({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+  } else if (!isNaN(inputDate)) {
+    inputDate = parseInt(inputDate);
+    res.send({ unix: inputDate, utc: new Date(inputDate).toUTCString() });
+  } else {
+    inputDate = new Date(inputDate);
+    if (inputDate !== "Invalid Date") {
+      res.send({ unix: inputDate.valueOf(), utc: inputDate });
+    } else {
+      res.send({ error: "Invalid Date" });
+    }
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
